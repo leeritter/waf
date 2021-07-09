23a0d0a9d357075a7338159bcafe17eb0fe61c56
Lee Ritter <lee.ritter@instrument.com>
2021-07-09 12:36:09 -0700

    adding direct upload functionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import DirectUploads from "./direct_uploads";
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+DirectUploads()
