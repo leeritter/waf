d858d08fc41c2d24200bcd03cdc845a0dd3c2fca
Lee Ritter <lee.ritter@instrument.com>
2021-07-19 08:19:50 -0700

    testing admin script

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import DirectUploads from "./direct_uploads";
+import DirectUploads from "./direct_uploads"
+require("packs/admin")
 
 Rails.start()
 Turbolinks.start()
