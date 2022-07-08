ac7a748560e0cf66c3bdde615b0d504550bbced4
Lee Ritter <lee@leeritter.com>
2022-07-07 23:03:18 -0700

    added grid to archive and custom select

--- README.md
+++ README.md
@@ -1,22 +1,27 @@
 # WORLD ARTS FOUNDATION INC
 
-This site is built with Ruby on Rails, and a React front-end using the `webpacker` gem to bundle and compile js files. 
+This site is built with Ruby on Rails, and a React front-end using the `webpacker` gem to bundle and compile js files.
 
 ### Routes
+
 Routes are handled as a SPA by react-router in the file: `app/javascript/routes/Index.jsx`
 
 ### Page Templates
+
 Page templates are jsx components that are hydrated with data from the API at: `app/controllers/api/v1/`
 
 ### Database
+
 This app is using a postgres database locally and in production.
 To create a local database you can run `rails db:create`, and then seed it with data using `rails db:seed`.
 
 ### Local Development
+
 - Make sure you have yarn installed globaly: `npm install --global yarn`
-- Install node modules: `npm install`
+- Install node modules: `yarn install`
 
 ### Deployment
+
 Staging and production environments are hosted on Heroku.
 To deploy the main branch run: `git push heroku main` to push to staging.
 
@@ -24,14 +29,15 @@ To deploy any other branch run `git push heroku <yourbranch>:master`.
 Migrate production ready code from staging to prod using Heroku's "promote" UI.
 
 ### Third Party Tools
-***Acts as taggable on*** <br/>
+
+**_Acts as taggable on_** <br/>
 Using this gem for tagging functionality on the `ArchiveItem` model. <br/>
 [https://github.com/mbleigh/acts-as-taggable-on](https://github.com/mbleigh/acts-as-taggable-on)
 
-***Devise*** <br/>
+**_Devise_** <br/>
 Used for account management and authentication for the CMS. <br/>
 [https://github.com/heartcombo/devise](https://github.com/heartcombo/devise)
 
-***Ransack*** <br/>
+**_Ransack_** <br/>
 Used for search. <br/>
 [https://github.com/activerecord-hackery/ransack](https://github.com/activerecord-hackery/ransack)
--- app/javascript/components/pages/ArchiveBeta.jsx
+++ app/javascript/components/pages/ArchiveBeta.jsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
+import Select from 'react-select'
 import { Link } from "react-router-dom";
 import Banner from "../Banner";
 import ArchiveItem from "../ArchiveItem";
+import TextBlock from "../TextBlock";
+import Footer from "../Footer";
+import Nav from "../Nav";
 
 const ArchiveBeta = () => {
     const [archiveResults, setArchiveResults] = useState([]);
+    const [filterYear, setFilterYear] = useState(null);
 
     function fetchResults() {
         const url = "/api/v1/archive_items/index";
@@ -26,23 +32,58 @@ const ArchiveBeta = () => {
         fetchResults();
     }, []);
 
+    const leftText = "<p>World Arts Foundation Inc will soon have a vast library of historic preserved media available to browse, here in the archive. Check back in the coming months to explore the full collection.</p>";
+    const options = [
+        { value: 'chocolate', label: 'Chocolate' },
+        { value: 'strawberry', label: 'Strawberry' },
+        { value: 'vanilla', label: 'Vanilla' }
+    ];
+
+    function handleYearSelect(val) {
+        console.log("change year", val);
+        setFilterYear(val);
+    }
+
     return (
         <div className="page-wrapper --is-dark">
-            <Banner themeLight={true} alignLeft={true} headline="The archive" />
-            <section>
-                <h1>Archive Items</h1>
-                <p>Count: {archiveResults.length}</p>
+            <Nav />
+            <Banner themeLight={false} alignLeft={true} headline="Albina<br/>Community Archive" className="--has-graphic --is-archive" />
+            <TextBlock themeLight={false} leftText={leftText} vPadding="lg" />
+            <section className="archive-wrapper global-container">
+                <div className="archive-content">
+                    <div className="archive-filters">
+                        <div className="archive-filters__col">
+                            <div className="archive-filters__label">Year</div>
+                            <Select
+                                placeholder="Select years..."
+                                value={filterYear}
+                                className="react-select-container"
+                                classNamePrefix="react-select"
+                                options={options}
+                                isSearchable={false}
+                                onChange={handleYearSelect}
+                            />
+                        </div>
+                    </div>
 
-                {archiveResults.map((item, index) => (
-                    <ArchiveItem
-                        key={index}
-                        year={item.year}
-                        title={item.title}
-                        medium={item.medium}
-                        content_files={item.content_files}
-                    />
-                ))}
+                    <ResponsiveMasonry
+                        columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
+                    >
+                        <Masonry gutter={20}>
+                            {archiveResults.map((item, index) => (
+                            <ArchiveItem
+                                key={index}
+                                year={item.year}
+                                title={item.title}
+                                medium={item.medium}
+                                content_files={item.content_files}
+                            />
+                        ))}
+                        </Masonry>
+                    </ResponsiveMasonry>
+                </div>
             </section>
+            <Footer />
         </div>
     );
 }
